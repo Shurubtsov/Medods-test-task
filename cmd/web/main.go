d14d7c6eba9d4d7e8b27ebfef022046b11dee8e4
Данила Шурубцов <qlyouqwa4ka@gmail.com>
2022-06-09 14:00:58 +0300

    add mongodb driver

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,21 +1,38 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/dshurubtsov/cmd/config"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 func main() {
 	// configuration flags before run app
 	addr := flag.String("addr", ":4000", "Net address HTTP")
+	dsn := flag.String("dsn", "mongodb://localhost:27017/", "Options for connect to MongoDB")
 
 	// configure loggers
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// context for db connection
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// client for mongo database
+	mongoClient, err := openMongoDB(*dsn, ctx)
+	if err != nil {
+		infoLog.Fatal(err.Error())
+	}
+	defer mongoClient.Disconnect(ctx)
 
 	// init application obj
 	app := &config.Application{
@@ -30,6 +47,29 @@ func main() {
 		Handler:  Routes(app),
 	}
 	infoLog.Printf("start server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openMongoDB(dsn string, ctx context.Context) (*mongo.Client, error) {
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(dsn))
+	if err != nil {
+		log.Fatal(err.Error())
+		return nil, err
+	}
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err.Error())
+		return nil, err
+	}
+
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err.Error())
+		return nil, err
+	}
+
+	return client, nil
 }
